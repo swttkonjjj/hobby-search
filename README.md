5c15255b3789d56d7f03fb27c78b8ea8ddd9f7d1
swtt <swttkonjjj@foxmail.com>
2022-09-10 14:35:20 +0800

    完善readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-readme
+# hobby_search兴趣搜索
+
+![icon](./img/icon256.png)
+
+>在兴趣爱好数据库中增加bilibli和百度搜索按钮
+
+# 安装
+greasfork：
+
+#源代码
+[github](https://github.com/swttkonjjj/hobby-search)
+[gitee](https://gitee.com/swttkonjjj/hobby-search)
--- hobby_search.js
+++ hobby_search.js
@@ -2,10 +2,10 @@
 // @name         hobby_search
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  在xin'q
+// @description  在兴趣爱好数据库中增加bilibli和百度搜索
 // @author       swtt
 // @match        http://hobby.lkszj.info/info?id=*
-// @icon         https://github.com/swttkonjjj/hobby-search/blob/master/img/icon.jfif
+// @icon         https://github.com/swttkonjjj/hobby-search/blob/master/img/search_icon.jfif
 // @grant        GM_addStyle
 // ==/UserScript==
 
@@ -21,11 +21,11 @@ function main() {
 
     element.innerHTML = '<div>' + biliSearch + baiduSearch + '</div>' + keyword
 
-    let bilicss = '#biliSearch { height: 45px;background-color: #00aeec;border-radius: 5px;color: white;font-size: large;'
-    let baiducss = '#baiduSearch { height: 45px;background-color: #4e6ef2;border-radius: 5px;color: white;font-size: large;'
+    let biliCss = '#biliSearch { height: 45px;background-color: #00aeec;border-radius: 5px;color: white;font-size: large;'
+    let baiduCss = '#baiduSearch { height: 45px;background-color: #4e6ef2;border-radius: 5px;color: white;font-size: large;'
 
-    GM_addStyle(bilicss)
-    GM_addStyle(baiducss)
+    GM_addStyle(biliCss)
+    GM_addStyle(baiduCss)
 }
 
 main()
